5b1d44ac8da3cc83de7c99f4d8b84df0dd863c01
Gennady Vedernikov <revoltlight@icloud.com>
2023-03-07 21:52:32 +0300

    finish project

--- FathersKickMarathonV3/ViewController.swift
+++ FathersKickMarathonV3/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var blueView: UIView!
+    @IBOutlet var slider: UISlider!
+    
+    let blueViewFrame: CGFloat = 100
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //Setup slider
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.value = 0
     }
 
-
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        let value = CGFloat(sender.value)
+        setSliderValue(value)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        blueView.layoutMargins = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        blueView.layoutMarginsDidChange()
+        slider.layoutMargins = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        slider.layoutMarginsDidChange()
+    }
+    
+    @IBAction func reverseSlider(_ sender: UISlider) {
+        
+        let maxValue = sender.maximumValue
+        
+        sender.setValue(maxValue, animated: true)
+        
+        UIView.animate(withDuration: 0.5, delay: 0) {
+            self.setSliderValue(CGFloat(maxValue))
+        }
+    }
+    
+    private func setSliderValue(_ value: CGFloat) {
+        
+        let layoutFrame = blueView.layoutMargins.left + blueView.layoutMargins.right + 20
+        
+        let translationX = (UIScreen.main.bounds.width - blueViewFrame - layoutFrame) * value
+        let rotation = value * .pi / 2
+        let scale = value * 0.5 + 1.0
+        
+        blueView.transform = CGAffineTransform.identity
+            .translatedBy(x: translationX, y: 0)
+            .rotated(by: rotation)
+            .scaledBy(x: scale, y: scale)
+    }
 }
 
