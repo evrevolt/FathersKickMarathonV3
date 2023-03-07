d23d7182f1bd600cae1ea65ab235bd8493ef96ea
Gennady Vedernikov <revoltlight@icloud.com>
2023-03-07 10:21:53 +0300

    Initial Commit

---
+++ FathersKickMarathonV3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FathersKickMarathonV3
+//
+//  Created by Геннадий Ведерников on 07.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
